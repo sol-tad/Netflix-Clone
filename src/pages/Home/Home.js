21db448bf652f6bf7aba9a0028930b029fa2afba
solabest <soltad65@gmail.com>
2024-02-24 10:45:52 -0700

    Adding Row componet of movies

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Banner from '../../components/Banner/Banner'
 import Footer from '../../components/Footer/Footer'
 import Header from "../../components/Header/Header"
-
+import RowList from '../../components/Rows/RowList/RowList'
 function Home() {
   return (
     <>
       <Header/>
       <Banner/>
+      <RowList/>
       <Footer/>
     </>
   )
