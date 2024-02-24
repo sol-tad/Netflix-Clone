90c010133c39ca179916746533759dc0e2ccb613
solabest <soltad65@gmail.com>
2024-02-23 18:29:35 -0700

    construct Header component

--- src/components/functional-component.js
+++
@@ -1,8 +0,0 @@
-// import react from 'react';
-// function Hellow(){
-// return <h1>Function component of react</h1>
-// }
-// export default Hellow;
-
-
-export const Hellow = () => <h1>Function component of react</h1>;
